1b00fbea39bea45c479e3646aafe43e35ac8835f
Lynn <clhuang224@gmail.com>
2024-04-07 17:24:27 +0800

    feat: logic

---
+++ README.md
@@ -0,0 +1,15 @@
+# vite-plugin-svg-sprite
+
+## Installation
+
+登入 GitHub Package Registry
+```
+pnpm login --scope=@clhuang224  --auth-type=legacy --registry=https://npm.pkg.github.com
+Username: <USERNAME>
+Password: <PERSONAL ACCESS TOKEN>
+```
+
+安裝
+```
+pnpm add -D github:clhuang224/vite-plugin-svg-sprite
+```
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,43 @@
 import { Plugin } from 'vite'
+import svgSpriter from 'svg-sprite'
+import fs from 'node:fs'
+import path from 'node:path'
+import { globSync } from 'glob'
 
-export default (): Plugin => {
+const VIRTUAL_ID = 'virtual:svg-sprite'
+const RESOLVED_ID = `\0${VIRTUAL_ID}`
+
+export default (options: { sourceDir: string, fileName: string }): Plugin => {
     return {
-        name: 'svg-sprite'
+        name: 'svg-sprite',
+        resolveId(id) {
+            if (id === VIRTUAL_ID) {
+                return RESOLVED_ID
+            }
+        },
+        load(id) {
+            if (id === RESOLVED_ID) {
+                return './assets/svg-sprite.svg'
+            }
+        },
+        generateBundle() {
+            const svgFiles = globSync(path.resolve(options.sourceDir, '**/*.svg'))
+            if (!svgFiles.length) return
+            const spriter = new svgSpriter({})
+            for (const svgFile in svgFiles) {
+                spriter.add(svgFile, null, fs.readFileSync(svgFile, { encoding: 'utf-8' }))
+            }
+            spriter.compile((error, result) => {
+                if (error) {
+                    console.error(error)
+                    return
+                }
+                this.emitFile({
+                    type: 'asset',
+                    fileName: 'svg-sprite.svg',
+                    source: result.contents
+                })
+            })
+        }
     }
 }
